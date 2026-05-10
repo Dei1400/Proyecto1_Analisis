1a0f3855798e51baadafa07b8ecce3c8c95c0d3f
DeyshmelinFallas <125696004+DeyshmelinFallas@users.noreply.github.com>
2026-05-09 21:56:27 -0600

    agregar carpetas

--- knights-tour/vite.config.js
+++ knights-tour/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://github.com/Dei1400/Proyecto1_Analisis.git',   //repo en GitHub
   plugins: [react()],
 })
